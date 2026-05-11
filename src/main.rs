3095cc5014908890762ca0e5140d9965a894ac41
AnthonyLons <anthonyguera551@gmail.com>
2026-05-11 14:38:01 -0400

    Use hyper for logic no tower

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,42 @@
-use std::convert::Infallible;
+mod routing;
+
 use std::net::SocketAddr;
 
-use http_body_util::Full;
+use http_body_util::BodyExt;
+use http_body_util::combinators::BoxBody;
 use hyper::body::Bytes;
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
+use hyper::{Method, StatusCode};
 use hyper::{Request, Response};
 use hyper_util::rt::TokioIo;
 use tokio::net::TcpListener;
 
-async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
-    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
+async fn echo(
+    req: Request<hyper::body::Incoming>,
+) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => Ok(Response::new(routing::full("Try POSTing data to /echo"))),
+        (&Method::GET, "/echo") => Ok(Response::new(req.into_body().boxed())),
+
+        _ => {
+            let mut not_found = Response::new(routing::empty());
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            Ok(not_found)
+        }
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-
     let listener = TcpListener::bind(addr).await?;
-
     loop {
         let (stream, _) = listener.accept().await?;
-
         let io = TokioIo::new(stream);
-
         tokio::task::spawn(async move {
             if let Err(err) = http1::Builder::new()
-                .serve_connection(io, service_fn(hello))
+                .serve_connection(io, service_fn(echo))
                 .await
             {
                 eprintln!("Error serving connection: {:?}", err);
---
+++ src/routing/mod.rs
@@ -0,0 +1,14 @@
+use http_body_util::BodyExt;
+use http_body_util::{Empty, Full, combinators::BoxBody};
+use hyper::body::Bytes;
+
+pub fn empty() -> BoxBody<Bytes, hyper::Error> {
+    Empty::<Bytes>::new()
+        .map_err(|never| match never {})
+        .boxed()
+}
+pub fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
+    Full::new(chunk.into())
+        .map_err(|never| match never {})
+        .boxed()
+}
